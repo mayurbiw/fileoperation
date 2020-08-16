1a12ef21b684e0c3ac67455bcbc7211edbea0765
mayurbiw <mayurbiw@gmail.com>
2020-08-17 01:06:29 +0530

    initial commit

---
+++ rename.py
@@ -0,0 +1,68 @@
+# Include standard modules
+import getopt, sys
+import os
+import datetime
+import collections
+
+#D:\Assignment\directory
+
+full_cmd_arguments = sys.argv
+argument_list = full_cmd_arguments[1:]
+
+print(argument_list)
+
+short_options = "f:"
+long_options = ["newfilename="]
+
+try:
+    arguments, values = getopt.getopt(argument_list, short_options, long_options)
+except getopt.error as err:
+    # Output error, and return with an error code
+    print (str(err))
+    sys.exit(2)
+
+file = dict()
+
+# the string used to rename the file
+renamestring = '0'
+
+path = 'd:/Assignment/directory'
+
+
+for current_argument, current_value in arguments:
+    if current_argument in ("-f", "--newfilename"):
+        print (("Enabling special output mode (%s)") % (current_value))
+        renamestring = current_value
+
+if renamestring == '0':
+    print("The command is wrong")
+
+#print("The rename string is"+renamestring)
+else:
+    path = path + '/' + renamestring
+    #print(path)
+    with os.scandir(path) as dir_contents:
+         for entry in dir_contents:
+            # print(entry)
+            if os.path.isfile(entry):
+                name = entry.name
+                info = entry.stat()
+                file[info.st_ctime] = entry
+
+    od = collections.OrderedDict(sorted(file.items(),reverse= True))
+
+    # printing files in  soreted order
+    for k, v in od.items():
+        print("The file names descending by date time")
+        print(datetime.datetime.fromtimestamp(k).strftime("%d %m %Y, %H:%M"), v.name)
+
+    # renaming the files
+    count = 1
+    for k,entry in od.items():
+        filename, file_extension = os.path.splitext(entry.name)
+        #print(path)
+        os.rename(entry, "d:/Assignment/directory/cats/"+renamestring[0:len(renamestring)-1] + str(count) + file_extension)
+        count = count + 1
+        file[k] = renamestring + str(count) + file_extension
+        print("Files after renaming")
+        print(datetime.datetime.fromtimestamp(k).strftime("%d %m %Y, %H:%M"), entry.name)
